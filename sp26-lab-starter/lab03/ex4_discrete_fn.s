3f11fef0751dacb3effc8489a75d543ec9b2edbd
Vincent <yong72426@gmail.com>
2026-08-09 21:36:24 +0800

    finished lab03

--- sp26-lab-starter/lab03/ex4_discrete_fn.s
+++ sp26-lab-starter/lab03/ex4_discrete_fn.s
@@ -6,6 +6,11 @@
 # The return value should be stored in a0
 f:
     # Your code here
+    addi t0 a0 3 #index = t0 = a0 + 3
+    slli t0 t0 2 #offset t1
+    add t0 t0 a1
+    lw a0 0(t0)
+    
 
     # This is how you return from a function. You'll learn more about this later.
     # This should be the last line in your program.
