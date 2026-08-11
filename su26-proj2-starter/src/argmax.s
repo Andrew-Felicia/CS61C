4630ab4bc6b59346dab10ec92bf861adaa1364d5
Vincent <yong72426@gmail.com>
2026-08-11 23:17:04 +0800

    working on task3

--- su26-proj2-starter/src/argmax.s
+++ su26-proj2-starter/src/argmax.s
@@ -7,7 +7,7 @@
 #   with the smallest index.
 # Arguments:
 #   a0 (int*) is the pointer to the start of the array
-#   a1 (int)  is the # of elements in the array
+#   a1 (int)  is the number of elements in the array
 # Returns:
 #   a0 (int)  is the first index of the largest element
 # Exceptions:
@@ -16,13 +16,28 @@
 # =================================================================
 argmax:
     # Prologue
+    li t0 1
+    blt a1, t0, error
+    li t1 0   #i = 0
+    
+    li t2  0 #the return index
+    
 
 
 loop_start:
+    bge t1 a1 loop_end
 
 
 loop_continue:
+    slli t3, t1, 2
+    add t3, t3, a0
+    lw t4 0(t3)
+    
 
+
+error:
+    li a0 36
+    j exit
 
 loop_end:
     # Epilogue
