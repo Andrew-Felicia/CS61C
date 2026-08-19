6b4715ca3b60cefac74304a2c2f2dd701b169991
Vincent <yong72426@gmail.com>
2026-08-19 18:11:33 +0800

    WorkingOn(proj2): working on proj2 partB task6

--- su26-proj2-starter/src/matmul.s
+++ su26-proj2-starter/src/matmul.s
@@ -23,41 +23,78 @@
 #   - If the dimensions of m0 and m1 don't match,
 #     this function terminates the program with exit code 38
 # =======================================================
+
+
+#paramter:
+# t0: outer loop counter
+# t1: inner loop counter
+# t3: the size of the result array.
+# t4: the insert index to result array.
+
+
 matmul:
 
     # Error checks
+    li t0 1
+    blt a1 t0 error
+    blt a2 t0 error
+    blt a4 t0 error
+    blt a5 t0 error
+    bne a2 a4 error
 
 
     # Prologue
+    mul t3 a1 a5 # t3:the size of the result array
+    mv t0 a1 # t0 is the outer loop counter.
+    li t4 0
 
+    j outer_loop_start
+
+error:
+    li a0 38
+    j exit
 
 outer_loop_start:
-
-
+    bge x0 t0 outer_loop_end
+    mv t1 a5 # t1 is the inner loop counter.
 
 
 inner_loop_start:
+    bge x0 t1 inner_loop_end
+    addi t1 t1 -1
 
+    #store value before call other functions.
+    addi sp sp -20
+    sw a0 0(sp)
+    sw a1 4(sp)
+    sw a2 8(sp)
+    sw a3 12(sp)
+    sw a4 16(sp)
 
+    #prepare arguments for function dot.
+    mv a1 a3
+    li a3 1
+    mv a4 a5
 
+    jal ra dot
 
+    sw a0 0(a6)
 
+    #restore value after call other functions.
+    lw a0 0(sp)
+    lw a1 4(sp)
+    lw a2 8(sp)
+    lw a3 12(sp)
+    lw a4 16(sp)
+    addi sp sp 20
 
-
-
-
-
-
+    j inner_loop_start
 
 inner_loop_end:
-
+    j outer_loop_start
 
 
 
 outer_loop_end:
-
-
     # Epilogue
-
-
     jr ra
